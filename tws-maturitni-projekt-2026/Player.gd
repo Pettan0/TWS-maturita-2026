6e068e8028e29b81379774ae205115210ad7174f
Pettan <pettancz@gmail.com>
2026-01-21 14:02:19 +0100

    Added Main menu

--- tws-maturitni-projekt-2026/Player.gd
+++ tws-maturitni-projekt-2026/Player.gd
@@ -15,7 +15,7 @@ func _unhandled_input(event: InputEvent) -> void:
 		if event is InputEventMouseMotion:
 			rotate_y(-event.relative.x * 0.002)
 			camera.rotate_x(-event.relative.y * 0.002)
-			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-30), deg_to_rad(60))
+			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-60), deg_to_rad(60))
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
